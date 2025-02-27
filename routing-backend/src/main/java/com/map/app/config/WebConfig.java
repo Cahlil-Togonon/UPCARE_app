7e8acb97fd7d9e3b672f5ce307676f73ec308c10
Cahlil-Togonon <agtogonon@up.edu.ph>
2025-02-28 03:02:25 +0800

    added CORS mapping

--- routing-backend/src/main/java/com/map/app/config/WebConfig.java
+++ routing-backend/src/main/java/com/map/app/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.map.app.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -11,6 +12,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 //@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
+@Override
+public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**") // This means all endpoints will be CORS-enabled
+            .allowedOrigins("http://localhost:3000") // The origin you want to allow
+            .allowedMethods("GET", "POST", "PUT", "DELETE") // Methods you want to allow
+            .allowedHeaders("*") // Allow all headers
+            .allowCredentials(true); // Allow credentials like cookies or authorization headers
+}
 
 @Override
 public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
